d6a4225c960da4f87da2dba62e0e02cac533ed21
Nerius <nerijus.ponomariovas@mif.stud.vu.lt>
2025-02-10 14:12:38 +0200

    Pridetos i my_library.h formules, kurios gauna duomenis apie studenta ir juos apskaiciuoja iskart

--- my_library.h
+++ my_library.h
@@ -1,15 +1,50 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
+#include <numeric>  
+#include <algorithm> 
 
 using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
+using std::cin;
 
 struct Student {
-    string name = "name";
-    string surname = "surname";
+    string name;
+    string surname;
     vector<int> grades;
-    int examGrade = 10;
+    int examGrade;
+
+
+void inputStudentData() {
+    cout << "Vardas: "; 
+    cin >> name;
+    cout << "Pavardė: "; 
+    cin >> surname;
+    cout << "Įveskite namų darbus (-1 baigti): ";
+    int grade;
+    while (cin >> grade && grade != -1) grades.push_back(grade);
+    cout << "Egzamino balas: "; 
+    cin >> examGrade;
+}
+double calculateAverage() {
+    if (grades.empty())
+        return 0;
+    return accumulate(grades.begin(), grades.end(), 0.0) / grades.size();
+}
+
+double calculateMedian() {
+    if (grades.empty()) 
+        return 0;
+    sort(grades.begin(), grades.end());
+    int n = grades.size();
+    return (n % 2 == 0) ? (grades[n / 2 - 1] + grades[n / 2]) / 2.0 : grades[n / 2];
+}
+
+double calculateFinalGrade(bool useMedian) {
+    return 0.4 * (useMedian ? calculateMedian() + 0.6 * examGrade : calculateAverage()) + 0.6 * examGrade;
+}
+
 };
