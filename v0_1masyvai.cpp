aef34b4fe79bdc83e477a2fb606a384490ef522c
Nerius <nerijus.ponomariovas@mif.stud.vu.lt>
2025-02-10 23:00:47 +0200

    Prideta nuskaitymo funkcija

--- v0_1masyvai.cpp
+++ v0_1masyvai.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 struct Student {
-    string name, surname;
+    string name;
+    string surname;
     int* grades;      // Dinaminis masyvas
     int examGrade;
     int hwCount = 0;  // Kiek buvo ivesta namu darbu
@@ -18,6 +19,53 @@ struct Student {
     ~Student() {
         delete[] grades; // Isvalome atminti sunaikinant objektą
     }
+
+    void inputStudentData() {
+        cout << "Vardas: ";
+        cin >> name;
+        cout << "Pavarde: ";
+        cin >> surname;
+    
+        // Dinaminis namu darbu ivedimas
+        cout << "Iveskite namu darbu balus (1-10). Iveskite -1, jei norite baigti: ";
+        while (hwCount < 100) {
+            int grade;
+            cin >> grade;
+        
+            if (cin.fail()) {  // Tikrina, ar buvo ivesta ne skaicius (Nusprendzia pagal kintamojo tipa)
+                cout << "Klaida! Iveskite skaiciu nuo 1 iki 10: ";
+                cin.clear();  // Isvalo klaidos busena
+                cin.ignore(1000, '\n');  // Pasalina netinkama ivesti is buferio
+                continue;  // Kartojame cikla
+            }
+        
+            if (grade == -1) 
+                break;
+        
+            if (grade >= 1 && grade <= 10) {
+                grades[hwCount++] = grade;  // Irašome i masyva
+            }
+            else {
+                cout << "Neteisingas balas! Kartokite (1-10): ";
+                cin.clear();  // Isvalo klaidos busena
+                cin.ignore(1000, '\n');  // Pasalina netinkama ivesti is buferio
+            }
+        }
+        cout << "Iveskite egzamino bala (1-10): ";
+        while (true) {
+            cin >> examGrade;
+            if (examGrade >= 1 && examGrade <= 10) {
+                break;
+            }
+            else {
+                cout << "Neteisingas skaicius! Kartokite (1-10): ";
+                cin.clear();
+                cin.ignore(100, '\n');
+            }
+        }
+
+    }
+
 };
 
     int main() {
