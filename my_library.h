d6029ac67a85fa08ccbdd744fcbbcefad1d6c90f
Nerius <nerijus.ponomariovas@gmail.com>
2025-03-02 13:04:58 +0200

    Pataisytos klaidos

--- my_library.h
+++ my_library.h
@@ -1,3 +1,6 @@
+#ifndef MY_LIBRARY_H // Patikrina, ar MY_LIBRARY_H dar nėra apibrėžtas
+#define FUNCTIONS_H // Jei nėra apibrėžtas, apibrėžia MY_LIBRARY_H (kad kitą kartą būtų atpažintas)
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -22,3 +25,4 @@ using std::sort;
 using std::accumulate;
 using std::istringstream;
 
+#endif  // Baigiame header guard (apsauga nuo pakartotinio įtraukimo)
