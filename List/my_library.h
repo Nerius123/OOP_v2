5c37b3b93f99d6d4aa71881f93e13127bf10c322
Nerius <nerijus.ponomariovas@gmail.com>
2025-03-17 16:10:55 +0200

    Sukurtas list folderis

---
+++ List/my_library.h
@@ -0,0 +1,37 @@
+#ifndef MY_LIBRARY_H // Patikrina, ar MY_LIBRARY_H dar nėra apibrėžtas
+#define MY_LIBRARY_H // Jei nėra apibrėžtas, apibrėžia MY_LIBRARY_H (kad kitą kartą būtų atpažintas)
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <numeric>  
+#include <algorithm> 
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <stdexcept>
+#include <random>
+#include <utility>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::cin;
+using std::ifstream;
+using std::ofstream;
+using std::setw;
+using std::fixed;
+using std::setprecision;
+using std::left;
+using std::sort;
+using std::accumulate;
+using std::istringstream;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::to_string;
+using std::pair;
+
+#endif  // Baigiame header guard (apsauga nuo pakartotinio įtraukimo)
