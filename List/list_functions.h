72fd7317ba9e7b5a4d94726d9226ed13dcfbe2c8
Nerius <nerijus.ponomariovas@gmail.com>
2025-03-20 00:33:32 +0200

    Perdaryta is vektor kodas i list

---
+++ List/list_functions.h
@@ -0,0 +1,30 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "../my_library.h"
+#include <list>  // Naudojame std::list vietoj std::vector
+
+// Studentų duomenys
+struct Student {
+    string name;
+    string surname;
+    list<int> grades;  // Pakeista iš vector<int> į list<int>
+    int examGrade;
+};
+
+// Funkcijų prototipai
+void inputStudentData(Student &s);
+double calculateAverage(const Student &s);
+double calculateMedian(const Student &s);
+double calculateFinalGrade(const Student &s, bool useMedian);
+void displayMenu();
+void printStudents(const list<Student> &students, bool useMedian);
+void readFromFile(list<Student> &students, const string &filename);
+void saveResultsToFile(list<Student> students, const string& filename, bool showAverage, bool showMedian);
+void generateStudentFiles();
+void splitStudentsIntoFiles(const list<Student>& students);
+void splitStudents(const list<Student>& students, list<Student>& vargsiukai, list<Student>& kietiakiai, bool useMedian);
+void saveStudentsToFile(const list<Student>& students, const string& filename);
+void testDataProcessing(const string& filename);
+
+#endif  // Baigiame header guard (apsauga nuo pakartotinio įtraukimo)
