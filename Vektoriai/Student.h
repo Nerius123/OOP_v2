ec63dcc7bb0011e39d4f18444f210dfbfe7726a2
Nerius <nerijus.ponomariovas@gmail.com>
2025-04-24 13:09:32 +0300

    Pataisyta student.h

--- Vektoriai/Student.h
+++ Vektoriai/Student.h
@@ -2,11 +2,9 @@
 
 #include "../my_library.h"
 
-class Student {
-private:
-    string name_; //rasau zodzio gale _ del to kad butu lengviau atskirti private kintamaji nuo funkcijos parametro
-    string surname_; // tas pats
-    vector<int> grades_; // tas pats
+class Student : public Zmogus {
+    private:
+    vector<int> grades_; //rasau zodzio gale _ del to kad butu lengviau atskirti private kintamaji nuo funkcijos parametro
     int examGrade_; // tas pats
 
 public:
@@ -29,21 +27,20 @@ public:
     Student& operator=(Student&& other) noexcept;
 
     // Geteriai (skaitymui)
-    inline string name() const { return name_; }
-    inline string surname() const { return surname_; }
-    inline const vector<int>& grades() const { return grades_; }
-    inline int exam() const { return examGrade_; }
+    const std::vector<int>& grades() const;
+    int exam() const;
 
     // Seteriai (keitimui)
-    inline void setName(const string& name) { name_ = name; }
-    inline void setSurname(const string& surname) { surname_ = surname; }
-    inline void setGrades(const vector<int>& grades) { grades_ = grades; }
-    inline void setExam(int examGrade) { examGrade_ = examGrade; }
+    void setGrades(const std::vector<int>& grades);
+    void setExam(int examGrade);
 
     // Naudojamos funkcijos
     double calculateAverage() const;
     double calculateMedian() const;
     double calculateFinalGrade(bool useMedian) const;
+
+    // Įgyvendinta abstrakti funkcija
+    void printInfo() const override;
 };
 
 // Lyginimo operatoriai (naudinga rikiavimui)
