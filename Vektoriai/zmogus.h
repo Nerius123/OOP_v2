89218640c52e9a4c32596fcc496ef1158600e5e7
Nerius <nerijus.ponomariovas@gmail.com>
2025-04-24 12:56:32 +0300

    Aprasyta klase zmogus

---
+++ Vektoriai/zmogus.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "../my_library.h"
+
+class Zmogus {
+protected:
+    string name_;
+    string surname_;
+
+public:
+    // Konstruktorius
+    Zmogus();
+    // Virtualus destruktorius
+    virtual ~Zmogus();  
+
+    // Kopijavimo konstruktorius
+    Zmogus(const Zmogus& other);  
+
+    // Kopijavimo priskyrimo operatorius
+    Zmogus& operator=(const Zmogus& other);  
+
+    // Move konstruktorius
+    Zmogus(Zmogus&& other) noexcept; 
+    
+    // Move priskyrimo operatorius
+    Zmogus& operator=(Zmogus&& other) noexcept; 
+
+    // Abstrakti funkcija (padaro klasę abstrakčia)
+    virtual void printInfo() const = 0;
+
+    // Geteriai
+    inline string name() const { return name_; }
+    inline string surname() const { return surname_; }
+
+    // Seteriai
+    inline void setName(const string& name) { name_ = name; }
+    inline void setSurname(const string& surname) { surname_ = surname; }
+};
